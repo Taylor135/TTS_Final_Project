56cc20340cd20e0d5787c1bdfd6310a468013c5d
Alex Fisher <fisherat@appstate.edu>
2018-07-31 17:10:01 -0400

    logic finished

--- app/controllers/dropdown_controller.rb
+++ app/controllers/dropdown_controller.rb
@@ -6,17 +6,58 @@ class DropdownController < ApplicationController
   def about_8
   end
 
-  def homework_6
+  def homework_6_1
   end
-  def homework_7
+  def homework_6_2
   end
-  def homework_8
+  def homework_6_3
+  end
+  def homework_6_4
   end
 
-  def solutions_6
+  def homework_7_1
   end
-  def solutions_7
+  def homework_7_2
   end
-  def solutions_8
+  def homework_7_3
   end
+  def homework_7_4
+  end
+
+  def homework_8_1
+  end
+  def homework_8_2
+  end
+  def homework_8_3
+  end
+  def homework_8_4
+  end
+
+  def solutions_6_1
+  end
+  def solutions_6_2
+  end
+  def solutions_6_3
+  end
+  def solutions_6_4
+  end
+
+  def solutions_7_1
+  end
+  def solutions_7_2
+  end
+  def solutions_7_3
+  end
+  def solutions_7_4
+  end
+
+  def solutions_8_1
+  end
+  def solutions_8_2
+  end
+  def solutions_8_3
+  end
+  def solutions_8_4
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,13 +16,36 @@ Rails.application.routes.draw do
   get 'dropdown/about_7' => 'dropdown#about_7'
   get 'dropdown/about_8' => 'dropdown#about_8'
 
-  get 'dropdown/homework_6' => 'dropdown#homework_6'
-  get 'dropdown/homework_7' => 'dropdown#homework_7'
-  get 'dropdown/homework_8' => 'dropdown#homework_8'
+  get 'dropdown/homework_6_1' => 'dropdown#homework_6_1'
+  get 'dropdown/homework_6_2' => 'dropdown#homework_6_2'
+  get 'dropdown/homework_6_3' => 'dropdown#homework_6_3'
+  get 'dropdown/homework_6_4' => 'dropdown#homework_6_4'
 
-  get 'dropdown/solutions_6' => 'dropdown#solutions_6'
-  get 'dropdown/solutions_7' => 'dropdown#solutions_7'
-  get 'dropdown/solutions_8' => 'dropdown#solutions_8'
+
+  get 'dropdown/homework_7_1' => 'dropdown#homework_7_1'
+  get 'dropdown/homework_7_2' => 'dropdown#homework_7_2'
+  get 'dropdown/homework_7_3' => 'dropdown#homework_7_3'
+  get 'dropdown/homework_7_4' => 'dropdown#homework_7_4'
+
+  get 'dropdown/homework_8_1' => 'dropdown#homework_8_1'
+  get 'dropdown/homework_8_2' => 'dropdown#homework_8_2'
+  get 'dropdown/homework_8_3' => 'dropdown#homework_8_3'
+  get 'dropdown/homework_8_4' => 'dropdown#homework_8_4'
+
+  get 'dropdown/solutions_6_1' => 'dropdown#solutions_6_1'
+  get 'dropdown/solutions_6_2' => 'dropdown#solutions_6_2'
+  get 'dropdown/solutions_6_3' => 'dropdown#solutions_6_3'
+  get 'dropdown/solutions_6_4' => 'dropdown#solutions_6_4'
+
+  get 'dropdown/solutions_7_1' => 'dropdown#solutions_7_1'
+  get 'dropdown/solutions_7_2' => 'dropdown#solutions_7_2'
+  get 'dropdown/solutions_7_3' => 'dropdown#solutions_7_3'
+  get 'dropdown/solutions_7_4' => 'dropdown#solutions_7_4'
+
+  get 'dropdown/solutions_8_1' => 'dropdown#solutions_8_1'
+  get 'dropdown/solutions_8_2' => 'dropdown#solutions_8_2'
+  get 'dropdown/solutions_8_3' => 'dropdown#solutions_8_3'
+  get 'dropdown/solutions_8_4' => 'dropdown#solutions_8_4'
 
   get 'grade/index'
 
